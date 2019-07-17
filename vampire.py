1d627a8a927cb26c9895f75e5b539b78345ca59a
adam-weiler <adam.weiler@yahoo.ca>
2019-07-17 19:14:51 -0400

    Creating file, adding basic psuedocode.

---
+++ vampire.py
@@ -0,0 +1,26 @@
+class Vampire():
+    #coven = []
+
+
+    #init
+        #name, age, in_coffin, drank_blood_today
+
+    #create()
+        #Add to coven
+
+    #drink_blood()
+        # drank_blood_today = true
+
+    #sunrise()
+        # for all vampires in coven
+            #if out_of_coffin or no drank blood today
+                # Remove from coffin
+
+    #sunset()
+        #for all vampires in coven
+            #drank_blood_today is false
+            #in_coffin is false
+
+
+    #go_home
+        #in_coffin to true
