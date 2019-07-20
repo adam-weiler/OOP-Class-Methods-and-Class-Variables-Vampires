4b0191185755a8e62572c20a71701814697cfeea
adam-weiler <adam.weiler@yahoo.ca>
2019-07-20 16:29:45 -0400

    Removing vampires from list, cleaning up file. Added list_all_vampires method.

--- vampire.py
+++ vampire.py
@@ -8,46 +8,44 @@ class Vampire():
         self.in_coffin = True
         self.drank_blood_today = True
 
-
-
-
-
-
-    @classmethod
-    def create(cls, name, age):
-        new_vampire = Vampire(name, age)
-        cls.coven.append(new_vampire)
-        return new_vampire
-
-
-
+    def __str__(self):
+        return f'The vampire {self.name} is {self.age}. In coffin: {self.in_coffin} - Drank blood today: {self.drank_blood_today}'
 
     def drink_blood(self):
         self.drank_blood_today = True
         return f'{self.name} has sated their appetite by drinking blood..\n'
 
-    #sunrise()
-        # for all vampires in coven
-            #if out_of_coffin or no drank blood today
-                # Remove from coffin
+    def go_home(self):
+        self.in_coffin = True
+        return f'{self.name} is returning to their coffin..\n'
 
-
+    #These are class methods.
+    @classmethod
+    def create(cls, name, age):
+        new_vampire = Vampire(name, age)
+        cls.coven.append(new_vampire)
+        print(f'{name} has joined the coven!')
+        return new_vampire
 
     @classmethod
+    def list_all_vampires(cls):
+        print('These are the active vampires in the coven..')
+
+        for vampire in cls.coven:
+            print(vampire)
+        
+    @classmethod
     def sunset(cls):
-        print('The sun is setting.. the vampires are all waking up...\n')
+        print('The sun is setting.. all the vampires are waking up...\n')
         for vampire in cls.coven:
             vampire.drank_blood_today = False
             vampire.in_coffin = False
 
-            # print(f'{vampire.name} - {vampire.in_coffin} - {vampire.drank_blood_today}')
-
-
     @classmethod
     def sunrise(cls):
         print('The sun is rising.. sunlight is deadly to vampires...\n')
-        for vampire in cls.coven:
-            if vampire.in_coffin and vampire.drank_blood_today:
+        for vampire in cls.coven[:]:
+            if (vampire.in_coffin == True) and (vampire.drank_blood_today == True):
                 print(f'{vampire.name} is safe and can rest until tomorrow night.')
             else:
                 cls.coven.remove(vampire)
@@ -56,42 +54,31 @@ class Vampire():
                 elif vampire.in_coffin == False:
                     print(f'{vampire.name} has burnt to death!')
                 else:
-                    print(f'{vampire.name} has been staked by a slayer!') #This probably won't run.
-                # print(f'{vampire.name} - {vampire.in_coffin} - {vampire.drank_blood_today}')
-        pass
+                    print(f'{vampire.name} has been staked by a slayer!') #This is just to close off the else condition. It won't be triggered.
 
 
-    def go_home(self):
-        self.in_coffin = True
-        return f'{self.name} is returning to their coffin..\n'
-
-# print(Vampire)
-
-dracula = Vampire.create('Dracula', 122)
-# print(f'{dracula.name} - {dracula.age} - {dracula.in_coffin} - {dracula.drank_blood_today}')
-
+dracula = Vampire.create('Dracula', 122) #Alive
 nosferatu = Vampire.create('Nosferatu', 97)
-
-blade = Vampire.create('Blade', 90)
-
+blade = Vampire.create('Blade', 90) #Alive
 barnabas = Vampire.create('Barnabas Collins', 175)
+von_count = Vampire.create('Count von Count', 1832652)  #Alive
+print()
 
-von_count = Vampire.create('Count von Count', 1832652)
-
-
+# Vampire.list_all_vampires()
+print()
 
 Vampire.sunset()
 
 print(dracula.drink_blood())
 print(dracula.go_home())
 
+print(nosferatu.go_home())
+
+print(barnabas.go_home())
 
 print(von_count.drink_blood())
 
-print(nosferatu.go_home())
-print(barnabas.go_home())
-
 Vampire.sunrise()
+print()
 
-
-print(Vampire.coven)
+Vampire.list_all_vampires()
